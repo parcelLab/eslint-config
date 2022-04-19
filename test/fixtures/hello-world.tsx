00e5f56cc77a1812ae90a9e4a071cbfeb30027e9
Carlos Santos <835129+cvscarlos@users.noreply.github.com>
2022-04-19 10:28:20 +0200

    simplify the file used for tests

--- test/fixtures/hello-world.tsx
+++
@@ -1,3 +0,0 @@
-export default function HelloWorld(): unknown {
-  return <div>Hello World</div> as unknown;
-}
---
+++ test/fixtures/sample-file.ts
@@ -0,0 +1,3 @@
+// This is just a sample file, because ESLint requires a file to run
+
+export default () => 'Hello World';
