c3c7d06403f564fe712a2483e3f81dd63c699c1f
Darío Blanco <dblancoit@gmail.com>
2022-08-31 17:48:26 +0200

    style: ignore abbreviation for ts

--- src/react-ts.js
+++ src/react-ts.js
@@ -2,6 +2,7 @@
  * Config base for projects using react with typescript.
  */
 
+// eslint-disable-next-line unicorn/prevent-abbreviations
 module.exports = {
   extends: [require.resolve("./react.js"), require.resolve("./typescript.js")],
   overrides: [
