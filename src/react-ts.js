59c79483d08d8edcf544f4b8527172a1463a4a96
Carlos <835129+cvscarlos@users.noreply.github.com>
2025-05-06 13:44:14 +0200

    feat: upgrade npm packages
    BREAKING CHANGE: not really breaking,
    but we replaced the abandoned `eslint-config-airbnb-typescript` package
    with `@kesills/eslint-config-airbnb-typescript` package,
    which is a fork of the original package.

--- src/react-ts.js
+++ src/react-ts.js
@@ -2,13 +2,12 @@
  * Config base for projects using react with typescript.
  */
 
-// eslint-disable-next-line unicorn/prevent-abbreviations
 module.exports = {
   extends: [require.resolve("./react.js"), require.resolve("./typescript.js")],
   overrides: [
     {
       files: ["*.ts?(x)"],
-      extends: ["airbnb-typescript", "prettier"],
+      extends: ["@kesills/airbnb-typescript", "prettier"],
       rules: {
         "react/require-default-props": "off",
       },
